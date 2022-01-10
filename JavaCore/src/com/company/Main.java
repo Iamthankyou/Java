1bb8b048cfd818fa7f003512fc5a29e75e0aae0f
Le Quang Duy <quangduynb00@gmail.com>
2022-01-10 14:51:56 +0700

    Complete app show-gemetry

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -14,31 +14,65 @@ public class Main {
             System.out.println("3. Draw the rectangle");
             System.out.println("0. Exit");
             System.out.println("Enter your choice: ");
+
             choice = input.nextInt();
+            int size = 0;
+
             switch (choice) {
                 case 1:
-                    System.out.println("Draw the triangle");
-                    System.out.println("******");
-                    System.out.println("*****");
-                    System.out.println("****");
-                    System.out.println("***");
-                    System.out.println("**");
-                    System.out.println("*");
+                    System.out.println("SIZE=");
+                    size = input.nextInt();
+                    System.out.println("Draw the triangle TOP LEFT");
+                    for (int i=0; i<size; i++){
+                        for (int j=0; j<size-i; j++){
+                            System.out.print("*");
+                        }
+                        System.out.println("");
+                    }
+
+                    System.out.println("Draw the triangle CENTER");
+
+                    for (int i = 1; i <= size; i++) {
+                        for (int j = size; j >= i; j--) {
+                            System.out.print(" ");
+                        }
+
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print("* ");
+                        }
+
+                        System.out.println();
+                    }
+
                     break;
                 case 2:
+                    System.out.println("SIZE=");
+                    size = input.nextInt();
+
                     System.out.println("Draw the square");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
+
+                    for (int i=0; i<size; i++){
+                        for (int j=0; j<size; j++){
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+
                     break;
                 case 3:
                     System.out.println("Draw the rectangle");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
-                    System.out.println("* * * * * *");
+                    System.out.println("w=");
+                    int w = input.nextInt();
+                    System.out.println("h=");
+                    int h = input.nextInt();
+
+                    for (int i=0; i<w; i++){
+                        for (int j=0; j<h; j++){
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+
                     break;
                 case 0:
                     System.exit(0);

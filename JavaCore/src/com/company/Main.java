c69b9b4c92fdb5606c74fe5a58976857289465ab
Le Quang Duy <quangduynb00@gmail.com>
2022-01-10 13:31:24 +0700

    Complete app usd to vnd

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-      
+        Scanner scan = new Scanner(System.in);
+
+        int x = 0;
+        do{
+            System.out.println("Nhập vào tỉ giá");
+            x = scan.nextInt();
+
+        }
+        while (x<0);
+
+        int usd = 0;
+        do{
+            System.out.println("Nhập vào số USD");
+            usd = scan.nextInt();
+
+        }
+        while (usd<0);
+
+        DecimalFormat formatter = new DecimalFormat("###,###,###");
+
+        System.out.println(formatter.format(usd*x)+" VNĐ");
     }
 }

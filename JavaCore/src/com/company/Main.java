349ef89e0e474777ca54215c6de0ee2932b7c334
Le Quang Duy <quangduynb00@gmail.com>
2022-01-10 14:26:11 +0700

    Commplete app GCD

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static int gcd(int a, int b){
+        return b==0?a:gcd(b,a%b);
+    }
+
     public static void main(String[] args) {
-      
+        int a,b;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("a=");
+        a = scan.nextInt();
+        System.out.println("b=");
+        b = scan.nextInt();
+
+        System.out.println("gcd(a,b) = " + gcd(a,b));
     }
 }

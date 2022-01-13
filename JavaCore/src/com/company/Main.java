959622d2e85cd7fe5bc974856f0db369e80666d8
Le Quang Duy <quangduynb00@gmail.com>
2022-01-13 08:35:59 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-      
+        String chuoi = "erjhksnvtrfgrgfuhkdghksdfksdfhsdlkf";
+        System.out.println("Typing the character: ");
+        Scanner scanner = new Scanner(System.in);
+        char a = scanner.next().charAt(0);
+        int count =0;
+        for (int i=0; i<chuoi.length();i++) {
+            if (chuoi.charAt(i) == a) {
+                count++;
+            }
+        }
+
+        System.out.println("Count: "+ count);
     }
 }

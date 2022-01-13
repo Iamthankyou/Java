42435f7d0f27444de00989430ca1a876b0fff702
Le Quang Duy <quangduynb00@gmail.com>
2022-01-13 16:02:34 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
-public class Main {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 
-    public static void main(String[] args) {
-      
+class CalculatorNextday {
+    public static String getNextDate(String curDate) {
+        String nextDate = "";
+
+        try {
+            Calendar today = Calendar.getInstance();
+            DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+            Date date = format.parse(curDate);
+            today.setTime(date);
+            today.add(Calendar.DAY_OF_YEAR, 1);
+            nextDate = format.format(today.getTime());
+        } catch (Exception e) {
+            return e.toString();
+        }
+        finally{
+            return nextDate;
+        }
     }
+
+
 }
---
+++ JavaCore/test/com/company/CalculatorNextdayTest.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import com.sun.tools.javac.Main;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorNextdayTest {
+    @Test
+    void testGetNextDate() {
+        String date = "01/01/2018";
+        String expected = "02/01/2018";
+        String result = CalculatorNextday.getNextDate(date);
+        assertEquals(expected, result);
+    }
+    @Test
+    void testGetNextDate2() {
+        String date = "31/01/2018";
+        String expected = "01/02/2018";
+        String result = CalculatorNextday.getNextDate(date);
+        assertEquals(expected, result);
+    }
+    @Test
+    void testGetNextDate3() {
+        String date = "30/04/2018";
+        String expected = "01/05/2018";
+        String result = CalculatorNextday.getNextDate(date);
+        assertEquals(expected, result);
+    }
+    @Test
+    void testGetNextDate4() {
+        String date = "28/02/2018";
+        String expected = "01/03/2018";
+        String result = CalculatorNextday.getNextDate(date);
+        assertEquals(expected, result);
+    }
+    @Test
+    void testGetNextDate5() {
+        String date = "29/02/2020";
+        String expected = "01/03/2020";
+        String result = CalculatorNextday.getNextDate(date);
+        assertEquals(expected, result);
+    }
+    @Test
+    void testGetNextDate6() {
+        String date = "31/12/2018";
+        String expected = "01/01/2019";
+        String result = CalculatorNextday.getNextDate(date);
+        assertEquals(expected, result);
+    }
+}

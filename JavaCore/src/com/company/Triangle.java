24ab372e86698574cb14e21573ad9dec6b399752
Le Quang Duy <quangduynb00@gmail.com>
2022-01-13 16:45:52 +0700

    Complete app

---
+++ JavaCore/src/com/company/Triangle.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Triangle {
+    public static int checkExistTriangle(int a, int b, int c) {
+        if (a + b <= c || a + c <= b || b + c <=a) {
+            return 0;
+        }
+        else if (a == b  && b == c) {
+            return 1;
+        }
+        else if (a == b || a == c || b == c) {
+            return 2;
+        }
+        else {
+            return 3;
+        }
+    }
+}

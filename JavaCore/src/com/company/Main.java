3931749b1f00dd799ef0115ee0c1ad3d5e3c97a0
Le Quang Duy <quangduynb00@gmail.com>
2022-01-13 09:04:31 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-      
+        Fan fan1 = new Fan(3,true,"red",20);
+        Fan fan2 = new Fan(2,false,"orange",20);
+        System.out.println(fan1);
+        System.out.println(fan2);
     }
 }

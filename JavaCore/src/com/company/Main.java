e5e3871020e7e36d05dd4bd1d6976768e6230cba
Le Quang Duy <quangduynb00@gmail.com>
2022-01-13 08:31:12 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-      
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("M= ");
+        int m = scanner.nextInt();
+        System.out.println("N=: ");
+        int n = scanner.nextInt();
+
+        int [][] a = new int[m][n];
+
+        for (int i =0; i<m; i++) {
+            for (int j=0;j<n;j++) {
+                System.out.println("A["+i+"]["+j+"]=");
+                a[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.print("Typing column you will need to sum: ");
+        int k = scanner.nextInt();
+        int sum = 0;
+
+        for (int i=0; i<m; i++) {
+            sum += a[i][k];
+        }
+
+        System.out.println("Sum: "+ sum);
     }
 }

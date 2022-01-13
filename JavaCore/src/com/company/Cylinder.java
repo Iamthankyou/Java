90c0f56cf924a5f2bdf80f7750019213f236bed3
Le Quang Duy <quangduynb00@gmail.com>
2022-01-13 09:23:47 +0700

    Complete app

---
+++ JavaCore/src/com/company/Cylinder.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Cylinder extends  Circle{
+    private double height;
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return this.getRadius()*Math.PI*this.height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" +
+                "height=" + height +
+                '}';
+    }
+}

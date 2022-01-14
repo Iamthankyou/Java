9233c7467964f8ddb7a0e9c6ad6d1b3c18a0b330
Le Quang Duy <quangduynb00@gmail.com>
2022-01-14 09:23:04 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-      
+        HashMap<String, Integer> map = new HashMap<>();
+        String test = "mot hai mot hai mot mot mot mot hai";
+        test = test.toLowerCase(Locale.ROOT);
+
+        for (String i:test.split(" ")){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+
+        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+
+        System.out.println(": ");
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }

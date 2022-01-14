d7200bf924d5aec4b334c7e3a336ee95a18498d4
Le Quang Duy <quangduynb00@gmail.com>
2022-01-14 09:15:34 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-      
+        Stack<Integer> stack = new Stack<>();
+        int []a = {1,2,3,4,5};
+
+        for (int i:a) {
+            stack.push(i);
+        }
+
+        int i=0;
+        while (!stack.isEmpty())
+            a[i++] = stack.pop();
+        }
     }
 }

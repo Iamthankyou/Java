daa6f0ff59481145951f0da97ebeff91f8db406d
Le Quang Duy <quangduynb00@gmail.com>
2022-01-18 21:46:56 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-      
+        int tmp;
+        int a[] = {2,3,5,2,1,1,1,2,3,6,32,4,6};
+        int n = a.length;
+
+        for (int i=1; i<n; i++) {
+            int j=i;
+            while ((j>0) && (a[j]<a[j-1])) {
+                tmp = a[j];
+                a[j] = a[j-1];
+                a[j-1]=tmp;
+                j--;
+            }
+
+            System.out.println("The sorted: ");
+
+            for (int k=0; k<=i;k++) {
+                System.out.print(" "+ a[k]);
+            }
+
+            System.out.println();
+        }
     }
 }

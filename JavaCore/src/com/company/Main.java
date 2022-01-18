d82d4948ba14888164ecb3b6e39c29a9a8b7190a
Le Quang Duy <quangduynb00@gmail.com>
2022-01-18 21:48:44 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,6 +1,20 @@
 package com.company;
 
 public class Main {
+    static void insertSort(int [] list) {
+        int tmp;
+        int n = list.length;
+        for (int i=1; i<n; i++) {
+            int j=i;
+            while ((j>0) && (list[j]>list[j-1])) {
+                tmp = list[j];
+                list[j] = list[j-1];
+                list[j-1]=tmp;
+                j--;
+
+            }
+        }
+    }
 
     public static void main(String[] args) {
       

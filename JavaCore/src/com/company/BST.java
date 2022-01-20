7b12c822037a328b3c82545372abd80a42306933
Le Quang Duy <quangduynb00@gmail.com>
2022-01-20 08:53:48 +0700

    Update

--- JavaCore/src/com/company/BST.java
+++ JavaCore/src/com/company/BST.java
@@ -1,2 +1,60 @@
-package com.company;public class BST {
+package com.company;
+
+import java.util.ArrayList;
+
+public class BST<T extends Comparable<T>> {
+    private ArrayList<T> tree;
+
+    public BST(int size){
+        tree =  new ArrayList<>(size);
+//        for (int i=0; i<size; i++){
+//            tree.add(null);
+//        }
+    }
+
+    public void insert(T d){
+        int i = 0;
+        while (i < tree.size() && tree.get(i) != null) {
+            if (d.compareTo(tree.get(i)) < 0)
+                i = (2 * i) + 1;
+            else
+                i = (2 * i) + 2;
+        }
+
+        tree.set(i,d);
+    }
+
+    public int find(T d) {
+        int i = 0;
+        boolean found = false;
+
+        while (i < tree.size()) {
+            if (tree.get(i).compareTo(d) < 0)
+                i = (2 * i) + 1;
+            else if (d.compareTo(tree.get(i)) > 0)
+                i = (2 * i) + 2;
+            else {
+                found = true;
+                break;
+            }
+        }
+
+        if (found){
+            System.out.println(d + " found at " + i + ".");
+            return i;
+        }
+        else {
+            System.out.println(d + " not found.");
+            return 0;
+        }
+    }
+
+    public void traverse(int i) {
+        if (i < tree.size() && tree.get(i) != null) {
+            traverse((2 * i) + 1);
+            System.out.print(tree.get(i) + " ");
+            traverse((2 * i) + 2);
+        }
+    }
+
 }
--- JavaCore/test/com/company/BSTTest.java
+++ JavaCore/test/com/company/BSTTest.java
@@ -1,17 +1,54 @@
 package com.company;
 
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
-class BSTTest {
+
+class BSTTest extends TestCase {
+
+    BST<Integer> bst = new BST<>(100);
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
 
     @org.junit.jupiter.api.Test
     void insert() {
+        bst.insert(8);
+        bst.insert(7);
+        bst.insert(9);
+        bst.insert(7);
+
+        bst.traverse(0);
+
+        Assert.assertEquals("7 7 8 9", outputStreamCaptor.toString()
+                .trim());
     }
 
     @org.junit.jupiter.api.Test
     void find() {
+        Integer x = new Integer(8);
+        int actual = bst.find(x);
+        int expected = 2;
+        // 0 0 0 0 0 0 0 0 0 0
+        // 7 7 8
+
+        Assert.assertEquals(actual,expected);
     }
 
     @org.junit.jupiter.api.Test
     void traverse() {
+        Assert.assertEquals("7 7 8 9", outputStreamCaptor.toString()
+                .trim());
     }
 }

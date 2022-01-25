91398439072852d1440c8ec7fde7a349207eef01
Le Quang Duy <quangduynb00@gmail.com>
2022-01-25 13:34:53 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,22 +1,13 @@
 package com.company;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
-    public static int DISTANCE = 100;
-
-    public static int STEP = 2;
-
     public static void main(String[] args) {
-        Car carA = new Car("A");
-        Car carB = new Car("B");
-        Car carC = new Car("C");
-
-        Thread thread1 = new Thread(carA);
-        Thread thread2 = new Thread(carB);
-        Thread thread3 = new Thread(carC);
-
-        System.out.println("Distance: 100KM");
-        thread1.start();
-        thread2.start();
-        thread3.start();
+        String regex = "^[CAP][0-9][A-Za-z0-9]*[GHIKLM]$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher("P0123M");
+        System.out.println(matcher.matches());
     }
 }

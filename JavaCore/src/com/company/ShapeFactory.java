e6f902bd784887ccae1ae26b6ed946584e29e592
Le Quang Duy <quangduynb00@gmail.com>
2022-01-28 06:22:14 +0700

    Complete app

---
+++ JavaCore/src/com/company/ShapeFactory.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class ShapeFactory {
+    public Shape getShape(String type) {
+        if (type.equals("circle")) {
+            return new Circle();
+        } else if (type.equals("rectangle")) {
+            return new Rectangle();
+        } else {
+            return new Square();
+        }
+    }
+}

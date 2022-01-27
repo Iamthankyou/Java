fe778697f3f05dbbf17216a19148255df44e145d
Le Quang Duy <quangduynb00@gmail.com>
2022-01-28 06:38:54 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,11 +1,18 @@
 package com.company;
 
+import com.codegym.CollectionUtils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
-    public static int DISTANCE = 100;
-
-    public static int STEP = 2;
-
     public static void main(String[] args) {
-
+        CollectionOperations co = new CollectionUtilsAdapter();
+        Client client = new Client(co);
+        List<Integer> sourceList = Arrays.asList(0, 1, 2, 3, 4, 5);
+        Set<Integer> targetSet = new HashSet<>(sourceList);
+        client.printMaxValue(targetSet);
     }
 }

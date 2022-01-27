db2a131a58caa0bc0d75bf1087347499b12dcc05
Le Quang Duy <quangduynb00@gmail.com>
2022-01-28 06:36:37 +0700

    Complete app

--- JavaCore/src/com/company/Main.java
+++ JavaCore/src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
 
+import java.util.List;
+
 public class Main {
-    public static int DISTANCE = 100;
-
-    public static int STEP = 2;
-
     public static void main(String[] args) {
-
+        RandomList randomList = new RandomList();
+        List<Integer> numbers = randomList.generateList(20,1,10);
+        ListFilter listFilter = new ListFilter();
+        numbers = listFilter.filterOdd(numbers);
+        ListPrinter listPrinter = new ListPrinter();
+        listPrinter.printList(numbers);
     }
 }

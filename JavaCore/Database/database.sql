402f39f12e1a29fc2f5235c0d55da8d52f5bd118
Le Quang Duy <quangduynb00@gmail.com>
2022-02-07 14:12:48 +0700

    Complete app

--- JavaCore/Database/database.sql
+++ JavaCore/Database/database.sql
@@ -1,30 +1,65 @@
 
-CREATE DATABASE management;
-USE management;
 
-CREATE TABLE Customer
+CREATE DATABASE QuanLySinhVien;
+USE QuanLySinhVien;
+CREATE TABLE Class
 (
-    cID   INT AUTO_INCREMENT PRIMARY KEY,
-    cName VARCHAR(60),
-    cAge INT,
+    ClassID   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ClassName VARCHAR(60) NOT NULL,
+    StartDate DATETIME    NOT NULL,
+    Status    BIT
 );
-CREATE TABLE Order
+CREATE TABLE Student
 (
-    oID INT AUTO_INCREMENT PRIMARY KEY,
-    cID INT FORGEIN KEY REFERENCES Customer(cID),
-    oDate DATE,
-    oTotalPrice DOUBLE
+    StudentId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    StudentName VARCHAR(30) NOT NULL,
+    Address     VARCHAR(50),
+    Phone       VARCHAR(20),
+    Status      BIT,
+    ClassId     INT         NOT NULL,
+    FOREIGN KEY (ClassId) REFERENCES Class (ClassID)
 );
-CREATE TABLE Product
+CREATE TABLE Subject
 (
-    pID INT AUTO_INCREMENT PRIMARY KEY,
-    pName varchar(60),
-    pPrice DOUBLE
+    SubId   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubName VARCHAR(30) NOT NULL,
+    Credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( Credit >= 1 ),
+    Status  BIT                  DEFAULT 1
 );
-CREATE TABLE OrderDetai;l
+
+CREATE TABLE Mark
 (
-    oID INT,
-    pID INT,
-    odQTY INT,
-    PRIMARY KEY(oID,pID),
-); 
+    MarkId    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubId     INT NOT NULL,
+    StudentId INT NOT NULL,
+    Mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
+    ExamTimes TINYINT DEFAULT 1,
+    UNIQUE (SubId, StudentId),
+    FOREIGN KEY (SubId) REFERENCES Subject (SubId),
+    FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
+);
+
+INSERT INTO Class
+VALUES (1, 'A1', '2008-12-20', 1);
+INSERT INTO Class
+VALUES (2, 'A2', '2008-12-22', 1);
+INSERT INTO Class
+VALUES (3, 'B3', current_date, 0);
+
+INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
+INSERT INTO Student (StudentName, Address, Status, ClassId)
+VALUES ('Hoa', 'Hai phong', 1, 1);
+INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+VALUES ('Manh', 'HCM', '0123123123', 0, 2);
+
+INSERT INTO Subject
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+
+INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
+VALUES (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1);

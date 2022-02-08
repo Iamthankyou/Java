9b31a95eed2d155d80f6f0365afe228f4afb70ee
Le Quang Duy <quangduynb00@gmail.com>
2022-02-08 09:52:55 +0700

    Complete app

--- JavaCore/Database/database.sql
+++ JavaCore/Database/database.sql
@@ -1,28 +1,71 @@
 DELIMITER //
 
-CREATE PROCEDURE findAllCustomers()
+CREATE PROCEDURE getCusById
+
+(IN cusNum INT(11))
 
 BEGIN
 
-  SELECT * FROM customers;
+  SELECT * FROM customers WHERE customerNumber = cusNum;
 
 END //
 
 DELIMITER ;
 
-call findAllCustomers();
+call getCusById(175);
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS `findAllCustomers`//
 
-CREATE PROCEDURE findAllCustomers()
+CREATE PROCEDURE GetCustomersCountByCity(
+
+    IN  in_city VARCHAR(50),
+
+    OUT total INT
+
+)
 
 BEGIN
 
-SELECT * FROM customers where customerNumber = 175;
+    SELECT COUNT(customerNumber)
 
-END //
+    INTO total
+
+    FROM customers
+
+    WHERE city = in_city;
+
+END//
+
 DELIMITER ;
 
+CALL GetCustomersCountByCity('Lyon',@total);
 
-call findAllCustomers();
+SELECT @total;
+
+DELIMITER //
+
+CREATE PROCEDURE SetCounter(
+
+    INOUT counter INT,
+
+    IN inc INT
+
+)
+
+BEGIN
+
+    SET counter = counter + inc;
+
+END//
+
+DELIMITER ;
+
+SET @counter = 1;
+
+CALL SetCounter(@counter,1);
+
+CALL SetCounter(@counter,1);
+
+CALL SetCounter(@counter,5);
+
+SELECT @counter;

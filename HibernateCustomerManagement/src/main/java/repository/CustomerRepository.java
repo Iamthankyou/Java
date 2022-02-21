706b040c3f7c3ab0c62ad6d1d9ee835748da58c0
Le Quang Duy <quangduynb00@gmail.com>
2022-02-21 17:14:14 +0700

    Complete app

--- HibernateCustomerManagement/src/main/java/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Customer;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
-}
--- HibernateCustomerManagement/src/main/java/service/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-import repository.CustomerRepository;
-
-
-public interface CustomerService extends CustomerRepository {
-}

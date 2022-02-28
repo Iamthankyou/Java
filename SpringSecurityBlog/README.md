a5ba02db35adf9bca7cb1e7d7c106a28d5b83352
Le Quang Duy <quangduynb00@gmail.com>
2022-02-28 14:20:57 +0700

    Complete app

--- SpringSecurityBlog/README.md
+++
@@ -1,2 +0,0 @@
-# WBDS-Application-SpringSecurity_Blog
-[Bài tập] Tích hợp Spring Security cho ứng dụng Blog  with Spring MVC 2.0
--- SpringSecurityBlog/src/main/java/com/codegym/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.repositories;
-
-import com.codegym.models.Category;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CategoryRepository extends PagingAndSortingRepository<Category,Long> {
-}

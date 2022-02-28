47888432290effb17c3699c78b9a387e98a704b6
Le Quang Duy <quangduynb00@gmail.com>
2022-02-28 17:17:40 +0700

    Complete app

--- spring-jwt/src/main/java/com/codegym/configuration/SecurityInit.java
+++
@@ -1,8 +0,0 @@
-package com.codegym.configuration;
-
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
-
--- spring-jwt/src/main/java/com/codegym/service/role/IRoleService.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.service.role;
-
-import com.codegym.model.Role;
-import com.codegym.service.IGeneralService;
-
-public interface IRoleService extends IGeneralService<Role> {
-    Role findByName(String name);
-}
-
